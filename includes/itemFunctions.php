3aa2911e42df01d4815e7b3c87105701638f4395
Ed Hack <ed.hack@talktalk.net>
2015-03-07 18:28:16 +0000

    Checking whether location of db_con matters

--- includes/itemFunctions.php
+++ includes/itemFunctions.php
@@ -1,7 +1,7 @@
 <?php
-	include ($_SERVER['DOCUMENT_ROOT'] . '/dbconn.php');	
-	
+
 	function getNewestItems($itemNumber){
+	include ($_SERVER['DOCUMENT_ROOT'] . '/dbconn.php');
 		$query = "SELECT * FROM product ORDER BY price DESC;";
 		
 		if ($result = mysqli_query($db_con, $query)) {		//getting array of rows
