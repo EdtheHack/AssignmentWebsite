2f52010a538b4ba31ef4477a00b43a73e8d69a23
Ed Hack <ed.hack@talktalk.net>
2015-03-07 19:59:20 +0000

    Toying with syntax

--- includes/itemFunctions.php
+++ includes/itemFunctions.php
@@ -1,12 +1,12 @@
 <?php
 
 	function getNewestItems($itemNumber){
-	include ($_SERVER['DOCUMENT_ROOT'] . '/dbconn.php');
+		include ($_SERVER['DOCUMENT_ROOT'] . '/dbconn.php');
 		$query = "SELECT * FROM product ORDER BY price DESC;";
 		
 		if ($result = mysqli_query($db_con, $query)) {		//getting array of rows
 			$rows = array();
-			while($row = $db_con->mysqli_fetch_assoc($result))
+			while($row = mysqli_fetch_assoc($query))
 				{
 					$rows[] = $row;
 				}
