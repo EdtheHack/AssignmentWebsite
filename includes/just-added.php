55ed22db9c0af8639c19f39f796c521417b664ec
Ed Hack <ed.hack@talktalk.net>
2015-03-07 18:21:52 +0000

    Removing silly html placed in class

--- includes/just-added.php
+++ includes/just-added.php
@@ -12,29 +12,18 @@
 			$this->price = getNewestItems($itemNumber)[2];
 			$this->description = getNewestItems($itemNumber)[3];
 		}
-?>
-			<div class="row">
-			<br>
-			<div class="thumbnail">
-				<img src="http://placehold.it/320x150" alt="">
-				<div class="caption">
-					<h4 class="pull-right"><?php echo $price; ?></h4>
-					<h4>
-						<a href="#"><?php echo $name; ?></a>
-					</h4>
-					<p><?php echo $description; ?></p>
-				</div>
-				<div>
-					<button type="submit" class="btn btn-default">
-						<i class="fa fa-eye"></i> <b> View </b>
-					</button>
-					<button type="submit" class="btn btn-default pull-right">
-						<i class="fa fa-shopping-cart fa-1x"></i> <b> Add </b>
-					</button>
-				</div>
-			</div>
-		</div>
-		<?php
+		
+		public function getName(){
+			return $name;
+		}
+		
+		public function getPrice(){
+			return $price;
+		}
+		
+		public function getDescription(){
+			return $description;
+		}
 	}
 ?>
 
@@ -45,9 +34,9 @@
 		<h3>Just added</h3>
 		
 		<?php
-		$product1 = new MyClass;
-		$product2 = new MyClass;
-		$product3 = new MyClass;
+		$product1 = new product;
+		$product2 = new product;
+		$product3 = new product;
 		?>
 		
 		<div class="row">
@@ -55,11 +44,11 @@
 			<div class="thumbnail">
 				<img src="http://placehold.it/320x150" alt="">
 				<div class="caption">
-					<h4 class="pull-right">PRICE</h4>
+					<h4 class="pull-right"><?php echo $product1->getPrice();?></h4>
 					<h4>
-						<a href="#">Product</a>
+						<a href="#"><?php echo $product1->getName();?></a>
 					</h4>
-					<p>DESCRIPTION</p>
+					<p><?php echo $product1->getDescription();?></p>
 				</div>
 				<div>
 					<button type="submit" class="btn btn-default">
