7e087d638fdaf88b84445653e941efa8ecbd5490
ashleywm <ashleywm@outlook.com>
2015-03-07 16:45:22 +0000

    Updates to dev getting bootstrapped

--- dev/index.php
+++ dev/index.php
@@ -1,11 +1,3 @@
-<?php
-	include 'includes/databaseValidation.php';	
-	
-	if(isset($_SESSION['loggedIn']) && $_SESSION["stayLoggedIn"] == true){
-			echo "<script type=\"text/javascript\">document.location.href=\"userDetails.php\";</script>";
-	}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -19,204 +11,71 @@
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</head>
 <body>
-
+	<?php include ("includes/nav.php");?>
+	
 <div class="container">
-	<?php include ("includes/nav.php"); include ("includes/just-added.php")?>
-		
-	<div class="col-md-9">
-		<div class="well">
-						
-		<h3>Sign in</h3>
-			<br>
-			<form method="POST" action="">
-				<div class="form-group">
-					<input type="email" name="email" class="form-control" placeholder="Email"
-									<?php if(!empty($_POST["email"])){ echo " value='".$_POST["email"]."'"; }?>>
-									
-					<br> <input type="password" name="password" class="form-control" placeholder="Password"
-									<?php if(!empty($_POST["password"])){ echo " value='".$_POST["password"]."'"; }?>>
-							
-					<br>
-						<p id="p1" style="float: left" ><input type="checkbox" name="stayLoggedIn"> Remember Me</p>		
-						<p style="float: right"> <input type="submit" name="attemptLogin" class="btn btn-default" value="Login"> <a href ="resetPassword.php"><input class="btn btn-default" value="Forgotten Password"></a></p>
-		
-				</div>
-	            <br>
-			
-			</form>
-			<?php include ("includes/login.php")?>
-		</div>
-	</div>	
+	<?php include ("includes/just-added.php")?>
+    
+    <div class="col-md-9">
+    <div class="row">
+    <div class="jumbotron">
+  <h2>Guaranteed next day delivery...<br><small class="pull-right">(..or your next delivery is on us!)</small></h2>
+  <br>
+  <br>
+  <img src="http://placehold.it/750x200" alt="">
+  <br>
+ <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut mi non urna pellentesque vulputate vel nec sem. Nulla feugiat facilisis ex non finibus.</p>
+  <p><a class="btn btn-default btn-lg" href="#" role="button">Learn more</a></p>
 </div>
-			
-			<!-- End user login, begin user registration-
-
-			<div class="panel panel-default" style="background-color: #AAAAAA; margin-top: 10px; margin-bottom: 10px;">
-				<div class="panel-heading" role="tab">
-					
-					<a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-						<p style="text-align: center; margin-bottom: 0px; color: #333333; text-decoration: none">Register</p>
-					</a>
-					
-				</div>
-				<form method="POST" action="">
-					<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-						<div class="panel-body">
-							<div class="container-fluid">
-								<div class="row">
-									<div class="col-sm-3">
-										<p id="p1" class="text-left"><span class="label label-default">Email</span></p> 
-									</div>
-									<div class="col-sm-9">
-										<div class="center">
-											<span class="input-group"></span>
-											<input type="email" name="emailRegister" class="form-control" <?php if(!empty($_POST["emailRegister"])){ echo " value='".$_POST["emailRegister"]."'"; }?>>
-										</div>
-									</div>
-								</div>
-								
-								<div class="row">
-									<div class="col-sm-3">
-										<p id = "p1" class="text-left"><span class="label label-default">Password</span></p>
-									</div>
-									<div class="col-sm-9">
-										<div class="center">
-											<span class="input-group"></span>
-											<input type="password" name="passwordRegister" class="form-control" <?php if(!empty($_POST["passwordRegister"])){ echo " value='".$_POST["passwordRegister"]."'"; }?>>
-										</div>
-									</div>
-								</div>
-								
-								<div class="row">
-									<div class="col-sm-3">
-										<p id = "p1" class="text-left"><span class="label label-default">Retype Password</span></p>
-									</div>
-									<div class="col-sm-9">
-										<div class="center">
-											<span class="input-group"></span>
-											<input type="password" name="passwordRegisterCheck" class="form-control" <?php if(!empty($_POST["passwordRegisterCheck"])){ echo " value='".$_POST["passwordRegisterCheck"]."'"; }?>>
-										</div>
-									</div>
-								</div>
-
-								<div class="row">
-									<div class="col-sm-3">
-										<p id="p1" class="text-left"><span class="label label-default">First Name</span></p>
-									</div>
-									<div class="col-sm-9">
-										<div class="center">
-											<span class="input-group"></span>
-											<input type="text" name="firstName" class="form-control" <?php if(!empty($_POST["firstName"])){ echo " value='".$_POST["firstName"]."'"; }?>>
-										</div>
-									</div>
-								</div>
-
-								<div class="row">
-									<div class="col-sm-3">
-										<p id="p1" class="text-left"><span class="label label-default">Last Name</span></p>
-									</div>
-									<div class="col-sm-9">
-										<div class="center">
-											<span class="input-group"></span>
-											<input type="text" name="lastName" class="form-control" <?php if(!empty($_POST["lastName"])){ echo " value='".$_POST["lastName"]."'"; }?>>
-										</div>
-									</div>
-								</div>
-
-								<div class="row">
-									<div class="col-sm-3">
-										<p id="p1" class="text-left"><span class="label label-default">Address Line 1</span></p>
-									</div>
-									<div class="col-sm-9">
-										<div class="center">
-											<span class="input-group"></span>
-											<input type="text" name="addressLine1" class="form-control" <?php if(!empty($_POST["addressLine1"])){ echo " value='".$_POST["addressLine1"]."'"; }?>>
-										</div>
-									</div>
-								</div>
-
-								<div class="row">
-									<div class="col-sm-3">
-										<p id="p1" class="text-left"><span class="label label-default">Address Line 2</span></p>
-									</div>
-									<div class="col-sm-9">
-										<div class="center">
-											<span class="input-group"></span>
-											<input type="text" name="addressLine2" class="form-control" <?php if(!empty($_POST["addressLine2"])){ echo " value='".$_POST["addressLine2"]."'"; }?>>
-										</div>
-									</div>
-								</div>
-
-								<div class="row">
-									<div class="col-sm-3">
-										<p id="p1" class="text-left"><span class="label label-default">Mobile Number</span></p>
-									</div>
-									<div class="col-sm-9">
-										<div class="center">
-											<span class="input-group"></span>
-											<input type="text" name="mobileNumber" class="form-control" <?php if(!empty($_POST["mobileNumber"])){ echo " value='".$_POST["mobileNumber"]."'"; }?>>
-										</div>
-									</div>
-								</div>
-
-								<div class="row">
-									<div class="col-sm-3">
-										<p id="p1" class="text-left"><span class="label label-default">Home Number</span></p>
-									</div>
-									<div class="col-sm-9">
-										<div class="center">
-											<span class="input-group"></span>
-											<input type="text" name="homeNumber" class="form-control" <?php if(!empty($_POST["homeNumber"])){ echo " value='".$_POST["homeNumber"]."'"; }?>>
-										</div>
-									</div>
-								</div>
-								
-								<br>
-
-								<p style="text-align: center"> <input type="submit" name="attemptRegister" class="btn btn-default" value="Register"></p>
-								
-							</div>
-						</div>
-					</div>
-				</form>
-					
-				<?php 
-					if(isset($_POST['attemptRegister'])){
-						$email = $_POST['emailRegister'];
-						$password = $_POST['passwordRegister'];
-						$passwordCheck = $_POST['passwordRegisterCheck'];
-						$firstName = $_POST['firstName'];
-						$lastName = $_POST['lastName'];
-						$addressLine1 = $_POST['addressLine1'];
-						$addressLine2 = $_POST['addressLine2'];
-						$mobileNumber = $_POST['mobileNumber'];
-						$homeNumber = $_POST['homeNumber'];
-						
-						if ($password == $passwordCheck){
-							if (validateDetails($email, $password, $firstName, $lastName, $addressLine1, $addressLine2, $mobileNumber, $homeNumber) == 1){
-								if (createUser() == 1){
-									if (validateUser($email, $password) == 1){
-										$_SESSION["loggedIn"] = true;
-										echo "<script>window.location.replace(\"http://student20261.201415.uk/i7212753WebAssignment/userDetails.php\")</script>";
-									} else {
-									echo "problem registering";
-									}
-								} else {
-									echo "There was a problem registering";
-								}
-							} 						
-						} else {
-							echo "passwords do not match";
-						}
-					}
-				?>
-			</div>
-		</div>
-	</div>
-	
-	 Bootstrap -->
-	
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-	</body>
+</div>
+<div class="row">
+      <div class="well">
+        <h3>Our Latest Deals</h3>
+        <div class="row"> <br>
+        <div class="col-md-4">
+          <div class="thumbnail"> <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+              <h4 class="pull-right">PRICE</h4>
+              <h4><a href="#">Product</a> </h4>
+              <p>DESCRIPTION</p>
+            </div>
+            <div>
+              <button type="submit" class="btn btn-default"><i class="fa fa-eye"></i> <b> View </b></button>
+              <button type="submit" class="btn btn-default pull-right"><i class="fa fa-shopping-cart fa-1x"></i> <b> Add </b></button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail"> <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+              <h4 class="pull-right">PRICE</h4>
+              <h4><a href="#">Product</a> </h4>
+              <p>DESCRIPTION</p>
+            </div>
+            <div>
+              <button type="submit" class="btn btn-default"><i class="fa fa-eye"></i> <b> View </b></button>
+              <button type="submit" class="btn btn-default pull-right"><i class="fa fa-shopping-cart fa-1x"></i> <b> Add </b></button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="thumbnail"> <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+              <h4 class="pull-right">PRICE</h4>
+              <h4><a href="#">Product</a> </h4>
+              <p>DESCRIPTION</p>
+            </div>
+            <div>
+              <button type="submit" class="btn btn-default"><i class="fa fa-eye"></i> <b> View </b></button>
+              <button type="submit" class="btn btn-default pull-right"><i class="fa fa-shopping-cart fa-1x"></i> <b> Add </b></button>
+            </div>
+          </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
 </html>
