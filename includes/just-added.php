f4e5d7d614165fc1004729326a22ad0d365bdcf1
Ed Hack <ed.hack@talktalk.net>
2015-03-08 17:29:07 +0000

    Nesting the product serialization
    $this should now work

--- includes/just-added.php
+++ includes/just-added.php
@@ -28,37 +28,39 @@
 		
 		public function html(){
 			echo"
-		<div class=\"row\">
-			<br>
-			<div class=\"thumbnail\">
-				<img src=\"http://placehold.it/320x150\" alt=\"\">
-				<div class=\"caption\">
-					<h4 class=\"pull-right\">".$this->price."</h4>
-					<h4>
-						<a href=\"#\">".$this->name."</a>
-					</h4>
-					<p>".$this->description."</p>
+			<div class=\"row\">
+				<br>
+				<div class=\"thumbnail\">
+					<img src=\"http://placehold.it/320x150\" alt=\"\">
+					<div class=\"caption\">
+						<h4 class=\"pull-right\">".$this->price."</h4>
+						<h4>
+							<a href=\"#\">".$this->name."</a>
+						</h4>
+						<p>".$this->description."</p>
+					</div>
+					<div>
+						<form method=\"POST\" action=\"\">
+							<button type=\"submit\" name=\"viewProduct\" class=\"btn btn-default\">
+								<i class=\"fa fa-eye\"></i> <b> View </b>
+							</button>
+							<button type=\"submit\" class=\"btn btn-default pull-right\">
+								<i class=\"fa fa-shopping-cart fa-1x\"></i> <b> Add </b>
+							</button>
+						</form>
+					</div>
 				</div>
-				<div>
-					<form method=\"POST\" action=\"\">
-						<button type=\"submit\" name=\"viewProduct\" class=\"btn btn-default\">
-							<i class=\"fa fa-eye\"></i> <b> View </b>
-						</button>
-						<button type=\"submit\" class=\"btn btn-default pull-right\">
-							<i class=\"fa fa-shopping-cart fa-1x\"></i> <b> Add </b>
-						</button>
-					</form>
-				</div>
-			</div>
-		</div>";
+			</div>";
+		
+			if(isset($_POST['viewProduct'])){
+				$_POST['serializedProduct'] = serialize($this);
+				echo "<script type=\"text/javascript\">document.location.href=\"viewProduct.php\";</script>";
+			}
 		}
 		
 	}
 	
-	if(isset($_POST['viewProduct'])){
-		$_POST['serializedProduct'] = serialize($this);
-		echo "<script type=\"text/javascript\">document.location.href=\"viewProduct.php\";</script>";
-	}
+	
 ?>
 
 <div class="col-md-3">
