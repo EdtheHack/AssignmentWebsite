cda5201c69fdcb79eb703cae63a123662cde0c18
ashleywm <ashleywm@outlook.com>
2015-03-08 21:05:56 +0000

    removal of in active

--- change-account-details.php
+++ change-account-details.php
@@ -30,56 +30,53 @@ if ($_SESSION["loggedIn"] == true){
 		        <li class="active"><a data-toggle="tab" href="#sectionA">Change Password</a></li>
 		        <li><a data-toggle="tab" href="#sectionB">Change Email</a></li>
 		        <li><a data-toggle="tab" href="#sectionC">Change Other Details</a></li>
-		        </ul>
-		         	<div class="tab-content">
-		        		<div id="sectionA" class="tab-pane fade in active">	
-		        		<div>	
-								<h3>Change Password</h3>
+		   </ul>
+		     	<div class="tab-content">
+		       		<div id="sectionA" class="tab-pane fade in active">	
+						<h3>Change Password</h3>
+						<br>
+							<form method="POST" action="">
+								<div class="form-group">
+							     	<label for="email">Old Password :</label>
+									<input type="password" name="oldPassword" class="form-control" placeholder="Enter old password" <?php if(!empty($_POST["oldPassword"])){ echo " value='".$_POST["oldPassword"]."'"; }?>>
+								</div>			
 								<br>
-									<form method="POST" action="">
-										<div class="form-group">
-									     	<label for="email">Old Password :</label>
-											<input type="password" name="oldPassword" class="form-control" placeholder="Enter old password" <?php if(!empty($_POST["oldPassword"])){ echo " value='".$_POST["oldPassword"]."'"; }?>>
-										</div>			
-										<br>
-										<div class="form-group">
-									     	<label for="email">New Password :</label>
-											<input type="password" name="newPassword" class="form-control" placeholder="Enter new password" <?php if(!empty($_POST["password"])){ echo " value='".$_POST["password"]."'"; }?>>
-										</div>
-										<div class="form-group">
-									     	<label for="email">Confirm New Password :</label>
-											<input type="password" name="newPasswordCheck" class="form-control" placeholder="Confirm new password"  <?php if(!empty($_POST["passwordCheck"])){ echo " value='".$_POST["passwordCheck"]."'"; }?>>
-										</div>
-										<br>
-										<input type="submit" name="changePassword" class="btn btn-default" value="Save"> <input style="float: right;" type="submit" name="back" class="btn btn-default" value="Cancel"> 
-									</form>	
-									<br>
-									
-							</div> 
-							</div>            
-		        		<div id="sectionB" class="tab-pane fade in active">
-		        				<h3>Change your email address</h3>
+								<div class="form-group">
+							     	<label for="email">New Password :</label>
+									<input type="password" name="newPassword" class="form-control" placeholder="Enter new password" <?php if(!empty($_POST["password"])){ echo " value='".$_POST["password"]."'"; }?>>
+								</div>
+								<div class="form-group">
+							     	<label for="email">Confirm New Password :</label>
+									<input type="password" name="newPasswordCheck" class="form-control" placeholder="Confirm new password"  <?php if(!empty($_POST["passwordCheck"])){ echo " value='".$_POST["passwordCheck"]."'"; }?>>
+								</div>
 								<br>
-									<form method="POST" action="">
-										<div class="form-group">
-									     	<label for="email">Confirm Password:</label>
-											<input type="password" name="password" class="form-control" placeholder="Password" <?php if(!empty($_POST["password"])){ echo " value='".$_POST["password"]."'"; }?>>
-										</div>		
-										<br>	
-										<div class="form-group">
-									     	<label for="email">Old Email:</label>
-											<input type="email" name="oldEmail" class="form-control" placeholder="Old Email" <?php if(!empty($_POST["oldEmail"])){ echo " value='".$_POST["oldEmail"]."'"; }?>>
-										</div>
-										<div class="form-group">
-									     	<label for="email">New Email:</label>
-											<input type="email" name="newEmail" class="form-control" placeholder="New Email" <?php if(!empty($_POST["newEmail"])){ echo " value='".$_POST["newEmail"]."'"; }?>>
-										</div>
-											<br>
-											<input type="submit" name="back" class="btn btn-default" value="Back"> <input type="submit" name="changeEmail" class="btn btn-default" value="Save">
-									</form>	
-										<br>
+									<input type="submit" name="changePassword" class="btn btn-default" value="Save"> <input style="float: right;" type="submit" name="back" class="btn btn-default" value="Cancel"> 
+							</form>	
+							<br>
+						</div>            
+		        		<div id="sectionB" class="tab-pane fade">
+		        			<h3>Change your email address</h3>
+							<br>
+							<form method="POST" action="">
+								<div class="form-group">
+							     	<label for="email">Confirm Password:</label>
+									<input type="password" name="password" class="form-control" placeholder="Password" <?php if(!empty($_POST["password"])){ echo " value='".$_POST["password"]."'"; }?>>
+								</div>		
+								<br>	
+								<div class="form-group">
+							     	<label for="email">Old Email:</label>
+									<input type="email" name="oldEmail" class="form-control" placeholder="Old Email" <?php if(!empty($_POST["oldEmail"])){ echo " value='".$_POST["oldEmail"]."'"; }?>>
+								</div>
+								<div class="form-group">
+							     	<label for="email">New Email:</label>
+									<input type="email" name="newEmail" class="form-control" placeholder="New Email" <?php if(!empty($_POST["newEmail"])){ echo " value='".$_POST["newEmail"]."'"; }?>>
+								</div>
+								<br>
+									<input type="submit" name="back" class="btn btn-default" value="Back"> <input type="submit" name="changeEmail" class="btn btn-default" value="Save">
+							</form>	
+							<br>
 		        		</div>
-		        		<div id="sectionC" class="tab-pane fade in active">
+		        		<div id="sectionC" class="tab-pane fade">
 		        			why am i here 
 		        		</div>
 		        		</div>
