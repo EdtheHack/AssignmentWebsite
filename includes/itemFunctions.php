374d3fd0a85c8e821ebb68928937cc144142a216
Ed Hack <ed.hack@talktalk.net>
2015-03-08 11:01:25 +0000

    STILL diagnosing

--- includes/itemFunctions.php
+++ includes/itemFunctions.php
@@ -6,7 +6,7 @@
 		
 		if ($result = mysqli_query($db_con, $query)) {		//getting array of rows
 			$rows = array();
-			while($row = mysqli_fetch_assoc($result)) {
+			while($row = mysqli_fetch_array($result)) {
 					echo $row[1];
 					$rows[] = $row;
 					echo $rows[0];
