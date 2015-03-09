cd70e4c976e42c4a6640384e4da8282a6b8e657f
ashleywm <ashleywm@outlook.com>
2015-03-09 18:47:17 +0000

    Bootstrap Js wot u doin

--- login-page.php
+++ login-page.php
@@ -134,6 +134,9 @@
 
 </div>
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
 <script>
     $('#myTab a').click(function (e) {
         e.preventDefault();
@@ -151,7 +154,6 @@
     $('#myTab a[href="' + hash + '"]').tab('show');
 </script>
 				
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	
 	</body>
 </html>
