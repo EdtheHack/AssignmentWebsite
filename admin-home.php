bc6e8746dd4809a793cc74266a2062d6a8046d3c
jeremyclarke100 <jeremyclarke100@gmail.com>
2015-03-09 23:13:46 +0000

    Created a basic form for adding products

--- admin-home.php
+++ admin-home.php
@@ -58,24 +58,12 @@ include ("includes/nav.php");
 								<h4 class="panel-title">
 									<a data-toggle="collapse" data-parent="#accordion"
 										href="#collapseOne" aria-expanded="true"
-										aria-controls="collapseOne">View Orders
-                                        <span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>
-</a>
+										aria-controls="collapseOne">View Orders</a>
 								</h4>
 							</div>
 							<div id="collapseOne" class="panel-collapse collapse in"
 								role="tabpanel" aria-labelledby="headingOne">
-								<div class="panel-body">Anim pariatur cliche reprehenderit, enim
-									eiusmod high life accusamus terry richardson ad squid. 3 wolf
-									moon officia aute, non cupidatat skateboard dolor brunch. Food
-									truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-									tempor, sunt aliqua put a bird on it squid single-origin coffee
-									nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica,
-									craft beer labore wes anderson cred nesciunt sapiente ea
-									proident. Ad vegan excepteur butcher vice lomo. Leggings
-									occaecat craft beer farm-to-table, raw denim aesthetic synth
-									nesciunt you probably haven't heard of them accusamus labore
-									sustainable VHS.</div>
+								<div class="panel-body">Bare Orders.</div>
 							</div>
 						</div>
 
@@ -90,17 +78,29 @@ include ("includes/nav.php");
 							</div>
 							<div id="collapseTwo" class="panel-collapse collapse"
 								role="tabpanel" aria-labelledby="headingTwo">
-								<div class="panel-body">Anim pariatur cliche reprehenderit, enim
-									eiusmod high life accusamus terry richardson ad squid. 3 wolf
-									moon officia aute, non cupidatat skateboard dolor brunch. Food
-									truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon
-									tempor, sunt aliqua put a bird on it squid single-origin coffee
-									nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica,
-									craft beer labore wes anderson cred nesciunt sapiente ea
-									proident. Ad vegan excepteur butcher vice lomo. Leggings
-									occaecat craft beer farm-to-table, raw denim aesthetic synth
-									nesciunt you probably haven't heard of them accusamus labore
-									sustainable VHS.</div>
+								<div class="panel-body">
+                                <form>
+										<div class="form-group">
+											<label for="newProductName">Product Name</label> <input
+												type="text" class="form-control" id="newProductName"
+												placeholder="Enter product name">
+										</div>
+										<div class="form-group">
+											<label for="newProductPrice">Password</label> <input
+												type="number" class="form-control"
+												id="newProductPRice" placeholder="Enter product price">
+										</div>
+										<div class="form-group">
+											<label for="productDescription">Description</label> 
+											<textarea class="form-control" rows="5" id="newProductDescription"></textarea>
+										</div>
+										<div class="checkbox">
+											<label> <input type="listProductCheck"> List product immediately
+											</label>
+										</div>
+										<button type="submit" class="btn btn-default">Add Product</button>
+									</form>
+                                </div>
 							</div>
 						</div>
 
