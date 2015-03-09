70ec60395946422c0770da5bdfa66d8006ca76c3
Ed Hack <ed.hack@talktalk.net>
2015-03-09 22:11:22 +0000

    Syntax...

--- includes/just-added.php
+++ includes/just-added.php
@@ -27,7 +27,7 @@
 					</div>
 					<div>
 						<form method=\"POST\" action=\"viewProduct.php\">
-							<button type=\"submit\" name='product' value=".serialize($product)." class='btn btn-default left-margin'><i class=\"fa fa-eye\"></i> <b> View </b> </button>	
+							<button type=\"submit\" name='product' value=\"".serialize($product)."\" class='btn btn-default left-margin'><i class=\"fa fa-eye\"></i> <b> View </b> </button>	
 						</form>
 						<button type=\"submit\" class=\"btn btn-default pull-right\">
 							<i class=\"fa fa-shopping-cart fa-1x\"></i> <b> Add </b>
