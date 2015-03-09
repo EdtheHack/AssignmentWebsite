dab23b27ef657cf68978fed14c89bc0c4c0950d7
Ed Hack <ed.hack@talktalk.net>
2015-03-09 22:50:49 +0000

    Changing nesting of divs

--- includes/just-added.php
+++ includes/just-added.php
@@ -24,14 +24,13 @@
 							<a href=\"#\">".$product->getName()."</a>
 						</h4>
 						<p>".$product->getDescription()."</p>
-					</div>
-					<div>
-						<div class=\"col-md-5\">
+						
+						<div class=\"col-md-6\">
 							<form method=\"POST\" action=\"viewProduct.php\">
 								<button type=\"submit\" name='itemId' value='".$product->getId()."' class=\"btn btn-default left-margin\"><i class=\"fa fa-eye\"></i> <b> View </b> </button>	
 							</form>
 						</div>
-						<div class=\"col-md-5\">
+						<div class=\"col-md-6\">
 							<form method=\"POST\" action=\"viewProduct.php\">  
 								<button type=\"submit\" name='itemId' value='".$product->getId()."' class=\"btn btn-default pull-right\"><i class=\"fa fa-shopping-cart fa-1x\"></i> <b> Add </b> </button>	
 							</form>
