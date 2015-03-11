a9fc3e91db46fd493800cf1106c2aac8128571c6
Ed Hack <ed.hack@talktalk.net>
2015-03-11 21:18:16 +0000

    Removing include...

--- searchProducts.php
+++ searchProducts.php
@@ -4,7 +4,7 @@ ini_set ( 'display_errors', 1 );
 ini_set ( 'display_startup_errors', 1 );
 error_reporting ( - 1 );
 
-	include ("includes/product.php");
+	//include ("includes/product.php");
 	session_start();
 ?>
 
