c9061b35b0c35d17e004dff30e52ff3375c8678e
Ashley Morris <ashleywm@outlook.com>
2015-03-11 20:07:59 +0000

    MOre codeeeEEee

--- login-page.php
+++ login-page.php
@@ -159,6 +159,19 @@
     $('ul.nav-tabs > li > a').click( function(e) {
         history.pushState( null, null, $(this).attr('href') );
     });
+
+    $( document ).ready(function() {
+    	if (location.hash !== '') {
+    	    $('.nav-tabs a[href="' + location.hash.replace('tab_','') + '"]').tab('show');
+    	} else {
+    	    $('.nav-tabs a:first').tab('show');
+    	}
+
+    	$('.nav-tabs a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
+    	      window.location.hash = 'tab_'+  e.target.hash.substr(1) ; 
+    	      return false;
+    	});
+    });
 </script>
 				
 	
