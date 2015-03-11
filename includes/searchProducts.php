f56695993e2e7073d2389526923aa3f6a31f2396
Ashley Morris <ashleywm@outlook.com>
2015-03-11 19:39:48 +0000

    Added pound signs
    yeah ed, pounds signs

--- includes/searchProducts.php
+++ includes/searchProducts.php
@@ -32,7 +32,7 @@
 				<div class="thumbnail">
 					<img src="http://placehold.it/320x150" alt="">
 					<div class="caption">
-						<h4 class="pull-right"><?php echo $product->getPrice(); ?></h4>
+						<h4 class="pull-right"><?php echo "&pound;".round($product->getPrice(), 2); ?></h4>
 						<h4>
 							<a href="#"><?php echo $product->getName(); ?></a>
 						</h4>
