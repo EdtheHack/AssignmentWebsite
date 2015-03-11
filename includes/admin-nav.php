05b902fabc60c1a0bf13a884a97a9b7e59475315
jeremyclarke100 <jeremyclarke100@gmail.com>
2015-03-11 22:53:44 +0000

    Split up admin nav bar so easier to make more admin pages

---
+++ includes/admin-nav.php
@@ -0,0 +1,14 @@
+<div class="col-md-2">
+  <div class="row">
+    <ul class="nav nav-sidebar nav-pills nav-stacked">
+      <li><a href="#">Admin Home</a></li>
+      <li><a href="#">View All Orders</a></li>
+      <li><a href="#">Add New Product</a></li>
+      <li><a href="#">Edit Existing Product</a></li>
+      <li><a href="#">Remove Product</a></li>
+      <li><a href="#">Edit Customer Account Details</a></li>
+      <li><a href="#">Edit Your Account Details</a></li>
+    </ul>
+  </div>
+</div>
+<div class="col-md-1"> </div>
