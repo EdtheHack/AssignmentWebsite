977b94164de244e1e7602d6ceb8181df1ab9d0a9
Will Grime <willgeeee@gmail.com>
2015-03-11 17:56:00 +0000

    same as last time

--- viewProduct.php
+++ viewProduct.php
@@ -1,32 +1,36 @@
 <?php
-	session_start();
-	ini_set('display_errors',1);
-	ini_set('display_startup_errors',1);
-	error_reporting(-1);
-	
-	include ("includes/product.php");
-	include ("includes/common-functions.php");
-	
-	$row = getItem($_POST['itemId']);	
-	$product = new product($row[0], $row[1], $row[2], $row[3]);
-		
-	//$product = unserialize($_POST['product']);
+session_start ();
+ini_set ( 'display_errors', 1 );
+ini_set ( 'display_startup_errors', 1 );
+error_reporting ( - 1 );
+
+include ("includes/product.php");
+include ("includes/common-functions.php");
+
+$row = getItem ( $_POST ['itemId'] );
+$product = new product ( $row [0], $row [1], $row [2], $row [3] );
+
+// $product = unserialize($_POST['product']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Home - Web Programming Assignment 2</title>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="css/custom.css">
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
-		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-	</head>
+<head>
+<title>Home - Web Programming Assignment 2</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/custom.css">
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet"
+	href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
 <body>
 	<?php
-		include ("includes/nav.php");
+	include ("includes/nav.php");
 	?>
 	
 	
@@ -35,7 +39,7 @@
 			<div class="row">
 				<br>
 				<div class="col-md-6">
-					<img src="http://placehold.it/500x400" alt="">	
+					<img src="http://placehold.it/500x400" alt="">
 				</div>
 				<div class="col-md-4">
 					<h4 class="pull-right"><?php echo $product->getPrice();?></h4>
@@ -47,23 +51,25 @@
 					<button type="submit" class="btn btn-default pull-right">
 						<i class="fa fa-shopping-cart fa-1x"></i> <b> Add </b>
 					</button>
-				</div>	
-			</div>	
+				</div>
+			</div>
 		</div>
-	
-	
-<div class="row">
-	<div class="well">
-		<h3>Similar Products</h3>
-			<div class="col-md-5">
-					<img src="http://placehold.it/320x150" alt="">
+
+
+		<div class="row">
+			<div class="well">
+				<h3>Similar Products</h3>
+				<div class="row">
+					<div class="col-md-5">
+						<img src="http://placehold.it/320x150" alt="">
 						<div class="caption">
 							<h3>Product Name</h3>
 							<p>Description....</p>
 						</div>
+					</div>
 				</div>
 			</div>
+		</div>
 	</div>
-</div>
 </body>
 </html>
