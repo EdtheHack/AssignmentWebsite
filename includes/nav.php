c9d0a7c638e1c65a30c5f9f3aafed8e0e57f3d58
Ed Hack <ed.hack@talktalk.net>
2015-03-11 22:17:05 +0000

    Sorting general stuff

--- includes/nav.php
+++ includes/nav.php
@@ -6,10 +6,10 @@ error_reporting ( - 1 );
 if(basename($_SERVER['PHP_SELF']) == 'viewProduct.php') {
 
 } else if(basename($_SERVER['PHP_SELF']) == 'searchProducts.php'){
-	include ("common-functions.php");
-	include ("product.php");
+	include ("includes/common-functions.php");
+	include ("includes/product.php");
 } else{
-	include ("common-functions.php");
+	include ("includes/common-functions.php");
 }
 
 ?>
