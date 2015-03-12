9c4fb243b899b7f4a4c47d3a656f8dea98dfabbc
jeremyclarke100 <jeremyclarke100@gmail.com>
2015-03-12 20:52:03 +0000

    Started building product view/edit page

--- admin/admin-nav.php
+++ admin/admin-nav.php
@@ -3,9 +3,8 @@
     <ul class="nav nav-sidebar nav-pills nav-stacked">
       <li><a href="index.php">Admin Home</a></li>
       <li><a href="view-orders.php">View All Orders</a></li>
+      <li><a href="view-products.php">View/Edit All Products</a></li>
       <li><a href="add-product.php">Add New Product</a></li>
-      <li><a href="#">Edit Existing Product</a></li>
-      <li><a href="#">Remove Product</a></li>
       <li><a href="#">Edit Customer Account Details</a></li>
       <li><a href="#">Edit Your Account Details</a></li>
     </ul>
---
+++ admin/view-products.php
@@ -0,0 +1,84 @@
+<?php
+session_start ();
+
+/*
+ * include ("includes/common-functions.php");
+ *
+ * if (($_SESSION["loggedIn"] == true) && checkAdmin() == 1){
+ * //admin is logged in
+ * }else{
+ * echo "<script type=\"text/javascript\">document.location.href=\"login-page.php\";</script>";
+ * //FORCE USER TO LOG IN OR NOT ADMIN, IF LOGGED IN AND NOT ADMIN THEN THE LOGIN PAGE WILL SEND TO INDEX
+ * //(bit scrubby)
+ * }
+ */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>View/Edit Products - Web Programming Assignment 2</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/custom.css">
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet"
+	href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php
+include ("../includes/nav.php");
+?>
+<div class="container">
+  <div class="col-md-12">
+    <div class="row">
+      <div class="jumbotron">
+        <h2>View/Edit Products <small> A complete list of all produts, across the store.</small> </h2>
+        <br>
+        <p>Below is a complete list of all products.</p>
+      </div>
+    </div>
+    <?php
+include ("admin-nav.php");
+?>
+    <div class="col-md-9">
+      <table class="table table-hover table-responsive">
+       <table class="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Product Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Product 1</td>
+            <td>Edit Order</td>
+            <td>Delete Order</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Product 2</td>
+            <td>Edit Order</td>
+            <td>Delete Order</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Product 3</td>
+            <td>Edit Order</td>
+            <td>Delete Order</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+</body>
+</html>
