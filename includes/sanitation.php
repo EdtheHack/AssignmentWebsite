c6fa57ad0e1562ac15ab9c0e06b45787544f30c0
ashleywm <ashleywm@outlook.com>
2015-03-12 20:56:52 +0000

    stop return

--- includes/sanitation.php
+++ includes/sanitation.php
@@ -5,7 +5,7 @@ function sanitiseString ($string, $field, $min, $max){
 	echo "not reaching ";
 	
 	if (preg_match( '/^[A-Z 0-9 \'!@#$%&*_]{2,100}$/i', $string)) {
-		return $string;
+		//return $string;
 		echo "SUCEEESSSSSSS";
 	} else {
 		echo "<div class=\"alert alert-danger\">
