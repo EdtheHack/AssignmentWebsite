9596aeeea802d9df9796da160883e09c60de4cf5
ashleywm <ashleywm@outlook.com>
2015-03-12 22:54:59 +0000

    more regexing

--- includes/sanitation.php
+++ includes/sanitation.php
@@ -12,7 +12,7 @@ function sanitiseString ($string, $min, $max){
 
 function sanitiseCurrency ($input){
 
-	if (preg_match( '/^((?:\d{1,3}[,\.]?)+\d*)$/i', $input)) {    //format should be 1,100.12
+	if (preg_match('/^[0-9]+(?:\.[0-9]{0,2})?$/', $input)) {    //format should be 1,100.12
 		return 1;
 	} else {
 		return 0;
