c61445cfb4c8349622956e259d1b6616fb86436b
Ashley Morris <ashleywm@outlook.com>
2015-03-13 16:53:10 +0000

    Getting upload to work 1

--- admin/add-product.php
+++ admin/add-product.php
@@ -2,6 +2,7 @@
 session_start ();
 
 include ("../includes/sanitation.php");
+include ("../includes/add-product-functions.php");
 
 /*
  * include ("includes/common-functions.php");
@@ -74,6 +75,18 @@ include ("../includes/nav.php");
     			<div id="print_errors"></div> 
     			<br>
     			
+    			
+    									
+				<form action="" method="POST" enctype="multipart/form-data">
+					<div class="form-group">
+						<label for="newProductImage">Product Image</label> 
+						<input type="file" name="photo">
+						<p class="help-block">Please upload an image of the product here.</p>
+						<button type="submit" name="uploadPhoto" class="btn btn-default">Upload Image</button>
+					</div>
+				</form>
+	
+    			
 				<form method="POST" action="">
 					<div class="form-group">
 							<label for="newProductName">Product Name</label> <input
@@ -110,25 +123,18 @@ include ("../includes/nav.php");
 					</div>
                     
                     
-						<div class="form-group">
-						<form action="" method="POST" enctype="multipart/form-data">
-							<label for="newProductImage">Product Image</label> 
-							<input type="file" name="photo" value="1000000">
-							<p class="help-block">Please upload an image of the product here.</p>
-						</div>
-	
 						<div class="checkbox">
 							<label> <input type="checkbox" name="newProductList" value="true"> List product immediately
 							</label>
 						</div>
 						<button type="submit" name="newProduct" class="btn btn-default">Add Product</button>
-						</form>
+					
 					</form>
 				
 				
 				
 				<?php 
-					include ("../includes/add-product-functions.php");
+				
 				
 				?>
 				
