eecf176d07735d08733b27ede4e369228e1437d3
Ed Hack <ed.hack@talktalk.net>
2015-03-13 20:54:55 +0000

    Whoops

--- viewProduct.php
+++ viewProduct.php
@@ -66,7 +66,7 @@ $_SESSION['product'] = serialize($product);
 			<?php
 			$rows = getSimilarItems($product->getId());
 			
-			for ($i = 0; $i < count(rows); $i++) {
+			for ($i = 0; $i < count($rows); $i++) {
 				$similarProduct = new product($rows[$i][0], $rows[$i][1], $rows[$i][2], $rows[$i][3], $rows[$i][4], $rows[$i][6]);
 			?>
 					
