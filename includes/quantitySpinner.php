facd42c503f5754493322d6948729599958e935a
Will Grime <willgeeee@gmail.com>
2015-03-14 15:20:01 +0000

    Updated Spinner

--- includes/quantitySpinner.php
+++ includes/quantitySpinner.php
@@ -6,6 +6,7 @@
       <link href="http://code.jquery.com/ui/1.10.4/themes/ui-lightness/jquery-ui.css" rel="stylesheet">
       <script src="http://code.jquery.com/jquery-1.10.2.js"></script>
       <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+	  <script src="/resources/demos/external/jquery-mousewheel/jquery.mousewheel.js"></script>
   
   <style type="text/css">
 	#prodQtySpinner input {width: 100px}
@@ -13,10 +14,10 @@
   <script>
   
   $(function() {
-    var spinner = $( "#prodQtySpinner" ).spinner({
+   $( "#prodQtySpinner" ).spinner({
 		//icons:{down:"ui-icon-arrowthickstop-1-s" , up:"ui-icon-arrowthickstop-1-n"}
 		//incremental:false
-		min: 1
+		min: 1,
 		max: 15
 		}); 
 	})
@@ -28,4 +29,3 @@
 </div>
 </body>
 </html>
-	
