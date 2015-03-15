7e2c7edfab0ad3b9e2858484c734306135d459d6
Ashley Morris <ashleywm@outlook.com>
2015-03-15 17:54:03 +0000

    Update from string to int

--- includes/sanitisation.php
+++ includes/sanitisation.php
@@ -10,7 +10,7 @@ function sanitiseString ($string, $min, $max){
 }
 
 function sanitiseInteger($int){
-	if (preg_match( '/^[0-9]+$/', $string)) {
+	if (preg_match( '/^[0-9]+$/', $int)) {
 		return 1;
 	}else{
 		return 0;
