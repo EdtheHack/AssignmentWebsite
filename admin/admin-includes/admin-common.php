27ad9b25817657f33f8efc6b49f0287d53e7b28d
Ashley Morris <ashleywm@outlook.com>
2015-03-15 21:01:36 +0000

    Squares

--- admin/admin-includes/admin-common.php
+++ admin/admin-includes/admin-common.php
@@ -12,7 +12,7 @@ function getPage($pageId){
 		$stmt->execute ();
 		$stmt->bind_result ( $col0,  $col1,  $col2,  $col3,  $col4,  $col5,  $col6, $col7, $col8 );
 		$stmt->fetch();
-     	$rows[] = array( $col0,  $col1,  $col2,  $col3,  $col4,  $col5,  $col6,  $col7,  $col8 );
+     	$rows = array( $col0,  $col1,  $col2,  $col3,  $col4,  $col5,  $col6,  $col7,  $col8 );
 		$stmt->close ();
 	}
 		
