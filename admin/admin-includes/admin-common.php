e9e3738f1817ccb6103ecd3b911eb56c724ea532
Ashley Morris <ashleywm@outlook.com>
2015-03-15 21:00:18 +0000

    double close

--- admin/admin-includes/admin-common.php
+++ admin/admin-includes/admin-common.php
@@ -14,7 +14,6 @@ function getPage($pageId){
 		$stmt->fetch();
      	$rows[] = array( $col0,  $col1,  $col2,  $col3,  $col4,  $col5,  $col6,  $col7,  $col8 );
 		$stmt->close ();
-		$stmt->close ();
 	}
 		
 	$mysqli->close ();
