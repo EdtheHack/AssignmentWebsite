3f299c17b5dabac48e923d56bc4897c5c4a83a15
Ashley Morris <ashleywm@outlook.com>
2015-03-15 20:50:54 +0000

    Not fetching results

--- admin/edit-product.php
+++ admin/edit-product.php
@@ -66,6 +66,7 @@ include ("nav.php");
 					$stmt->bind_param ( "i", $productId );
 					$stmt->execute ();
 					$stmt->bind_result ( $col0,  $col1,  $col2,  $col3,  $col4,  $col5,  $col6, $col7, $col8 );
+					$stmt->fetch();
 					$stmt->close ();
 				}
 					
