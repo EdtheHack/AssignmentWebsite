346014a263011b99682f4273496bf0676a688135
Ed Hack <ed.hack@talktalk.net>
2015-03-16 20:00:27 +0000

    Fixed constructor

--- includes/order.php
+++ includes/order.php
@@ -7,7 +7,7 @@
 		public function __construct($id, $products, $confirmed){	
 			$this->id = $id;
 			for ($i = 0; $i < count($products); $i++) {
-				$product = new product($products[$i][0], $products[$i][1], $products[$i][2], $products[$i][3], $products[$i][4], $products[$i][5], $products[$i][6]);
+				$product = new product($products[$i][0], $products[$i][1], $products[$i][2], $products[$i][3], $products[$i][4], $products[$i][5], $products[$i][6], $products[$i][7], $products[$i][8]);
 				$this->products[$i] = $product;
 			}
 			$this->confirmed =$confirmed;
