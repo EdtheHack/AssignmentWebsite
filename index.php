96519caf4eb32df28fedb29e35cd347cfc553f0c
Ashley Morris <ashleywm@outlook.com>
2015-03-17 16:26:15 +0000

    Removing traces of will

--- index.php
+++ index.php
@@ -70,58 +70,7 @@ include ("includes/nav.php");
 					</div>
 				</div>
 			</div>
-    <?php
-				include ("includes/newest-products.php")?> // WILL FIX THIS ISSUE-LOVE WILL
-    
-    <!-- 
-<div class="row">
-      <div class="well">
-        <h3>Our Latest Deals</h3>
-        <div class="row"> <br>
-        <div class="col-md-4">
-          <div class="thumbnail"> <img src="http://placehold.it/320x150" alt="">
-            <div class="caption">
-              <h4 class="pull-right">PRICE</h4>
-              <h4><a href="#">Product</a> </h4>
-              <p>DESCRIPTION</p>
-            </div>
-            <div>
-              <button type="submit" class="btn btn-default"><i class="fa fa-eye"></i> <b> View </b></button>
-              <button type="submit" class="btn btn-default pull-right"><i class="fa fa-shopping-cart fa-1x"></i> <b> Add </b></button>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="thumbnail"> <img src="http://placehold.it/320x150" alt="">
-            <div class="caption">
-              <h4 class="pull-right">PRICE</h4>
-              <h4><a href="#">Product</a> </h4>
-              <p>DESCRIPTION</p>
-            </div>
-            <div>
-              <button type="submit" class="btn btn-default"><i class="fa fa-eye"></i> <b> View </b></button>
-              <button type="submit" class="btn btn-default pull-right"><i class="fa fa-shopping-cart fa-1x"></i> <b> Add </b></button>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <div class="thumbnail"> <img src="http://placehold.it/320x150" alt="">
-            <div class="caption">
-              <h4 class="pull-right">PRICE</h4>
-              <h4><a href="#">Product</a> </h4>
-              <p>DESCRIPTION</p>
-            </div>
-            <div>
-              <button type="submit" class="btn btn-default"><i class="fa fa-eye"></i> <b> View </b></button>
-              <button type="submit" class="btn btn-default pull-right"><i class="fa fa-shopping-cart fa-1x"></i> <b> Add </b></button>
-            </div>
-          </div>
-          </div>
-          </div>
-        </div>
-      </div>
-      
-      -->
+    <?php include ("includes/newest-products.php")?>
 
 		</div>
 	</div>
