67799c5cc7c09dae80929d2732152ca4c68fbce6
Ashley Morris <ashleywm@outlook.com>
2015-03-17 22:16:04 +0000

    Remove get page

--- admin/admin-includes/admin-common.php
+++ admin/admin-includes/admin-common.php
@@ -16,29 +16,6 @@ if(isset($_GET['del'])){ //if there is a deletion id
 }
 
 
-
-function getPage($pageId){
-	include ($_SERVER['DOCUMENT_ROOT'] . '/dbconn.php');
-		
-	$mysqli = $db_con;
-	
-	$rows = array();
-	
-	if ($stmt = $mysqli->prepare ("SELECT * FROM product WHERE product_id=?" )) {
-		$stmt->bind_param ( "i", $pageId );
-		$stmt->execute ();
-		$stmt->bind_result ( $col0,  $col1,  $col2,  $col3,  $col4,  $col5,  $col6, $col7, $col8 );
-		$stmt->fetch();
-     	$rows = array( $col0,  $col1,  $col2,  $col3,  $col4,  $col5,  $col6,  $col7,  $col8 );
-		$stmt->close ();
-	}
-		
-	$mysqli->close ();
-	return $rows;
-	
-}
-
-
 function deleteProductDB($product_id){
 	include ($_SERVER['DOCUMENT_ROOT'] . '/dbconn.php');
 
