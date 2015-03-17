8598f59110155c17fd72aa903d0f5f5c0bc2fb3c
jeremyclarke100 <jeremyclarke100@gmail.com>
2015-03-17 17:30:48 +0000

    Starting to create products page

---
+++ products.php
@@ -0,0 +1,38 @@
+<?php
+session_start ();
+
+ini_set ( 'display_errors', 1 );
+ini_set ( 'display_startup_errors', 1 );
+error_reporting ( - 1 );
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Products - Web Programming Assignment 2</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/custom.css">
+<link rel="stylesheet"
+	href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link rel="stylesheet"
+	href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script
+	src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php
+include ("includes/nav.php");
+?>
+<div class="container">
+  <?php include ("includes/just-added.php")?>
+  <div class="col-md-9">
+			
+
+		</div>
+	</div>
+	</div>
+</body>
+</html>
