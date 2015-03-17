45d2dd58791e44f836a63764dc917ba7fdbbbdae
jeremyclarke100 <jeremyclarke100@gmail.com>
2015-03-17 21:31:47 +0000

    Yeah trying insane crazy dumb stuff

--- view-all-products.php
+++ view-all-products.php
@@ -30,30 +30,37 @@ include ("includes/nav.php");
   <?php include ("includes/just-added.php");
 ?>
   <div class="col-md-9">
-  <div class="well">
+    <div class="well">
       <h3>All Products</h3>
       <?php
 	    		$rows = getAllProducts();
 				for($i = 0; $i < count($rows); $i++) {
 					
 				$product = new product ($rows[$i][0], $rows[$i][1], $rows[$i][2], $rows[$i][3], $rows[$i][4], $rows[$i][5], $rows[$i][6], $rows[$i][7], $rows[$i][8]);
-			?>
-      <div class="thumbnail"> <a href="viewProduct.php?<?php echo $product->getId(); ?>"> <img src="img/<?php echo $product->getImg(); ?>" alt="Image of one of our products" style="width:150px;height:auto"> </a>
-        <div class="caption">
-          <h4 class="pull-right">£<?php echo $product->getPrice(); ?></h4>
-          <h4><a href="viewProduct.php?<?php echo $product->getId(); ?>"><?php echo $product->getName(); ?></a></h4>
-          <p><?php echo $product->getDescription(); ?></p>
+				
+				if (count($rows) % 3 == 0) {
+					?>
+      <div class="row">
+        <?php 
+				}
+				?>
+        <div class="thumbnail"> <a href="viewProduct.php?<?php echo $product->getId(); ?>"> <img src="img/<?php echo $product->getImg(); ?>" alt="Image of one of our products" style="width:150px;height:auto"> </a>
+          <div class="caption">
+            <h4 class="pull-right">£<?php echo $product->getPrice(); ?></h4>
+            <h4><a href="viewProduct.php?<?php echo $product->getId(); ?>"><?php echo $product->getName(); ?></a></h4>
+            <p><?php echo $product->getDescription(); ?></p>
+          </div>
+          <div> <a href="viewProduct.php?<?php echo $product->getId(); ?>">
+            <button type="submit" class="btn btn-default"><i class="fa fa-eye"></i> <b> View </b></button>
+            </a>
+            <button type="submit" class="btn btn-default pull-right"><i class="fa fa-shopping-cart fa-1x"></i> <b> Add </b></button>
+          </div>
         </div>
-        <div> <a href="viewProduct.php?<?php echo $product->getId(); ?>">
-          <button type="submit" class="btn btn-default"><i class="fa fa-eye"></i> <b> View </b></button>
-          </a>
-          <button type="submit" class="btn btn-default pull-right"><i class="fa fa-shopping-cart fa-1x"></i> <b> Add </b></button>
-        </div>
-      </div>
+       
       <?php
 				}
 			?>
-
+    </div>
     </div>
   </div>
 </div>
