4706678570c85d6190c541c7d7e42a6b93361533
Ashley Morris <ashleywm@outlook.com>
2015-03-17 22:18:37 +0000

    Error reporting

--- admin/admin-includes/admin-common.php
+++ admin/admin-includes/admin-common.php
@@ -1,5 +1,10 @@
 <?php
 
+session_start();
+ini_set ( 'display_errors', 1 );
+ini_set ( 'display_startup_errors', 1 );
+error_reporting ( - 1 );
+
 
 if(isset($_GET['del'])){ //if there is a deletion id
 	$product_id = $_GET[ 'del' ]; //get the deletion id
