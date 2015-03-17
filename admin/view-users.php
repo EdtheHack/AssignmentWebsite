9fb85f4c77d2720d3ca8b124c542bf36097f0e98
Ashley Morris <ashleywm@outlook.com>
2015-03-17 22:22:06 +0000

    Nothing happening ?

--- admin/view-users.php
+++ admin/view-users.php
@@ -60,6 +60,7 @@ include ("nav.php");
 		    if(isset ($_GET['name'])){
 		    	$letter = $_GET['name']; 
 		    	$row = listNames($letter);
+		    	echo $row;
 		    	//$user = new user($row[0][0], $row[0][1], $row[0][2], $row[0][3], $row[0][4], $row[0][5], $row[0][6], $row[0][7], $row[0][8], $row[0][9], $row[0][10]);
 		    	echo $row[0][1]; 
 		    }
