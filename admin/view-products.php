0ffc35465ba038da8442570ba87e2f4fd3abe479
Ashley Morris <ashleywm@outlook.com>
2015-03-17 17:58:47 +0000

    Action post with id

--- admin/view-products.php
+++ admin/view-products.php
@@ -173,7 +173,7 @@ include ("nav.php");
 										product? This cannot be undone.</div>
 									<div class="modal-footer">
 										<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-										<form method="POST">
+										<form method="POST" action="delete-product.php?<?php echo $product->getId();?>">
 											<button type="submit" name="del" class="btn btn-danger"  >Delete Product</button>
 										</form>
 									</div>
@@ -185,12 +185,6 @@ include ("nav.php");
 						
 					
 					<?php 
-							
-							
-							if(isset($_POST['del'])){
-								echo $product->getId();
-							}
-							
 							}
 							
 					?>
