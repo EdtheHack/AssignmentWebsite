11b9a33487a17bb76044ea3f83659f6086925b15
Ashley Morris <ashleywm@outlook.com>
2015-03-17 15:33:24 +0000

    (!(TESTING

--- admin/edit-product.php
+++ admin/edit-product.php
@@ -201,9 +201,7 @@ include ("nav.php");
 						<label for="newProductImage">Product Image</label> <br> 
 						<input type="file" name="photo" >
 						<p class="help-block">Please upload an image of the product here.</p>
-						<p class="help-block">NOTE: By not uploading another photo by clicking browse, the current photo below will be kept as the desired product photo in the catelogue.</p>
-						
-						
+						<p class="help-block">NOTE: By not uploading another photo by clicking browse, the current photo below will be kept as the desired product photo in the catelogue.</p>						
 					</div>
 				</div>
 				<div class="col-md-6">
