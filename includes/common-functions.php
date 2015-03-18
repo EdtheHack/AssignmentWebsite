a6110788cf3eaaf5990c1941c07efdae56847427
Ed Hack <ed.hack@talktalk.net>
2015-03-18 10:48:55 +0000

    Ugh name change

--- includes/common-functions.php
+++ includes/common-functions.php
@@ -239,7 +239,7 @@ function getCurrentOrderProducts($orderId){
 	return $rows;
 }
 
-function getProductQuantities($currentOrderId){
+function getProductQuantities($orderId){
 	$mysqli = connect ();
 	
 	echo "ID".$currentOrderId;
