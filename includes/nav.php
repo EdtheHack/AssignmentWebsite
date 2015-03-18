22a8c875026d90205777cae00a8f82c42ce48e9b
jeremyclarke100 <jeremyclarke100@gmail.com>
2015-03-18 17:01:51 +0000

    adding dropdown to nav

--- includes/nav.php
+++ includes/nav.php
@@ -22,6 +22,18 @@ include ("includes/user.php");
 			<ul class="nav navbar-nav">
 				<li><a href="index.php"><i class="fa fa-home"></i> Home </a></li>
 				<li><a href="view-all-products.php"><i class="fa fa-square-o"></i> Products </a></li>
+                <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Categories <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
 			</ul>
 			<form method="POST" action="searchProducts.php?currentPage=1" class="navbar-form navbar-left">
 				<div class="form-group">
