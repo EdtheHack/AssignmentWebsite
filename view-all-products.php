9aa4e15501efc4acb19c9e976a4462e46777b6ce
Ashley Morris <ashleywm@outlook.com>
2015-03-18 00:09:51 +0000

    spanning

--- view-all-products.php
+++ view-all-products.php
@@ -40,11 +40,11 @@ include ("includes/nav.php");
 				$product = new product ($rows[$i][0], $rows[$i][1], $rows[$i][2], $rows[$i][3], $rows[$i][4], $rows[$i][5], $rows[$i][6], $rows[$i][7], $rows[$i][8]);
 				
 				if($count == 3){
-					echo "<div class=\"row\">";
+					echo "<div class=\"row-fluid\">";
 				}
 				
 				?>
-				<div class="col-md-4">
+				<div class="span4">
         <div class="thumbnail"> <a href="viewProduct.php?<?php echo $product->getId(); ?>"> <img src="img/<?php echo $product->getImg(); ?>" alt="Image of one of our products" style="width:150px;height:auto"> </a>
           <div class="caption">
             <h4 class="pull-right">£<?php echo $product->getPrice(); ?></h4>
