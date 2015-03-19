4252bbbf68fb0ee22eeff0a938246362d9aeebe4
Ed Hack <ed.hack@talktalk.net>
2015-03-19 12:26:00 +0000

    Syntax

--- searchProducts.php
+++ searchProducts.php
@@ -39,11 +39,11 @@ if (isset($_POST['searchItem'])){$_SESSION['searchItem'] = $_POST['searchItem'];
 	?>
 		<div class="well">
 			<div class="row">
-				<div class="col-md-3">
+				<div class="col-md-6">
 					<h4><?php echo "\"".$_SESSION['searchItem']."\" - ".getAllSearchItems($_SESSION['searchItem'])." Items"; ?></h4>
 				</div>
-				<div class="col-md-3">
-					<h4><?php echo "Page "$currentPage." Products";?></h4>
+				<div class="col-md-6">
+					<h4><?php echo "Page ".$currentPage." Products";?></h4>
 				</div>
 			</div>
 			<br>
