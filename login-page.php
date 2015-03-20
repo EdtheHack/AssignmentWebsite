be8723e8e10b336d2459e8f1f8f8bdaf807c73e1
Ashley Morris <ashleywm@outlook.com>
2015-03-20 01:44:37 +0000

    JS issue ?

--- login-page.php
+++ login-page.php
@@ -14,8 +14,8 @@
 		<link rel="stylesheet" href="css/custom.css">
 		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>-->
 	</head>
 <body>
 	<?php include ("includes/nav.php");?>
