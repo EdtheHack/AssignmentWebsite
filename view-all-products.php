11168ad50ed81dcc1c81d5dde5fa3e2f833ecc3e
Will Grime <willgeeee@gmail.com>
2015-03-20 16:28:29 +0000

    trying some layout stuff

--- view-all-products.php
+++ view-all-products.php
@@ -46,18 +46,19 @@ session_start ();
 				?>
 				<div class="col-md-4">
 					<div class="thumbnail"> 
-						<h5><?php if ($product->getPercentage() == 0){
-							echo "<strong> &pound;".$product->getPrice()."</strong>";
-						} else {
-							$salePriceTmp = number_format(($product->getPrice() * $product->getPercentage() / 100), 2, '.', '');
-							$salePrice =  number_format(($product->getPrice() - $salePriceTmp), 2, '.', '');
-							echo "<strike>&pound;".$product->getPrice()."</strike>  ".$product->getPercentage()."% off! <br> <strong> Our Price: &pound;".$salePrice."</strong> <br>";
-						} ?> </h5>
+						
 						<a href="viewProduct.php?<?php echo $product->getId(); ?>"> <img src="img/<?php echo $product->getImg(); ?>" alt="Image of one of our products" style="width:150px;height:150px"> </a>
 						<div class="caption">
 							<h4><a href="viewProduct.php?<?php echo $product->getId(); ?>"><?php echo $product->getName(); ?></a></h4>
 							<p><?php echo $des ?></p>
 						</div>
+						<span class="pull-right"></span><h5><?php if ($product->getPercentage() == 0){
+							echo "<strong> &pound;".$product->getPrice()."</strong>";
+						} else {
+							$salePriceTmp = number_format(($product->getPrice() * $product->getPercentage() / 100), 2, '.', '');
+							$salePrice =  number_format(($product->getPrice() - $salePriceTmp), 2, '.', '');
+							echo "<strike>&pound;".$product->getPrice()."</strike>  ".$product->getPercentage()."% off! <br> <strong> Our Price: &pound;".$salePrice."</strong> <br>";
+						} ?> </h5></span>
 						<div> 
 							<a href="viewProduct.php?<?php echo $product->getId(); ?>"> <button type="submit" class="btn btn-default"><i class="fa fa-eye"></i> <b> View </b></button></a>
 						</div>
