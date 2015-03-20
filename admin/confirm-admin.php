4ee188036dac2e99d92dbb0f5a166283b7e8164e
Ashley Morris <ashleywm@outlook.com>
2015-03-20 03:05:35 +0000

    include dir issue

--- admin/confirm-admin.php
+++ admin/confirm-admin.php
@@ -27,7 +27,7 @@ include ("../includes/common-functions.php");
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</head>
 <body>
-	<?php include ("includes/nav.php");?>
+	<?php include ("nav.php");?>
 	
 <div class="container">
 <br>	
