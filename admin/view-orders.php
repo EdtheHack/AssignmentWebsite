4b84dbaf4c9fb45920a105164482d076de638587
Ed Hack <ed.hack@talktalk.net>
2015-03-21 16:04:49 +0000

    Adding delete modal

--- admin/view-orders.php
+++ admin/view-orders.php
@@ -82,6 +82,27 @@ include ("admin-nav.php");
 					?>
 					</tbody>
 				</table>
+				  	<!-- Modal -->
+				<div class="modal fade" id="myModal" tabindex="-1" role="dialog"
+					aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal"
+									aria-label="Close">
+									<span aria-hidden="true">&times;</span>
+								</button>
+								<h4 class="modal-title" id="myModalLabel">Delete Order</h4>
+							</div>
+							<div class="modal-body">Are you sure you want to delete this
+								order? This cannot be undone.</div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+								<button type="button" class="btn btn-danger">Delete Order</button>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 			<br>
 			<?php
@@ -113,30 +134,6 @@ include ("admin-nav.php");
 				</table>
     </div>
   </div> -->
-  
-  	<!-- Modal -->
-	<div class="modal fade" id="myModal" tabindex="-1" role="dialog"
-		aria-labelledby="myModalLabel" aria-hidden="true">
-		<div class="modal-dialog">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal"
-						aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-					<h4 class="modal-title" id="myModalLabel">Delete Order</h4>
-				</div>
-				<div class="modal-body">Are you sure you want to delete this
-					order? This cannot be undone.</div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-danger">Delete Order</button>
-				</div>
-			</div>
-		</div>
-	</div>
-	<?php }
-	?>
 </div>
 </body>
 </html>
