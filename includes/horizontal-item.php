fb6f15ac21ece11d617e3feb2dc213b2663a932e
Ed Hack <ed.hack@talktalk.net>
2015-03-22 19:06:41 +0000

    Trying to do more cheeky code reuse

---
+++ includes/horizontal-item.php
@@ -0,0 +1,23 @@
+<div class="well">
+	<div class="row">
+		<div class="col-md-3">
+		<a href="viewProduct.php?<?php echo $product->getId(); ?>">
+			<img src="includes/<?php echo $product->getImg(); ?>" alt="Image of a product found from your search query" height="150" width="150">
+			</a>
+		</div>
+		<div class="col-md-9">
+			<h5 class="pull-right"><?php if ($product->getPercentage() == 0){
+				echo "<strong> &pound;".$product->getPrice()."</strong>";
+			} else {
+				$salePriceTmp = number_format(($product->getPrice() * $product->getPercentage() / 100), 2, '.', '');
+				$salePrice =  number_format(($product->getPrice() - $salePriceTmp), 2, '.', '');
+				echo "<strong> Our Price: &pound;".$salePrice."</strong> RRP: <strike>&pound;".$product->getPrice() ."</strike><br>";
+			} ?> </h5> <!-- PLEASE IGNORE HTML ERRORS -->
+			<h4><a href="viewProduct.php?<?php echo $product->getId(); ?>"><?php echo $product->getName(); ?></a></h4>
+			<p> <?php echo $product->getDescription(); ?></p>
+			<a href="viewProduct.php?<?php echo $product->getId(); ?>"><button type="submit" name='itemId' value='<?php echo $product->getId(); ?>' class="btn btn-default right-margin"><i class="fa fa-eye"></i> <b> View </b> </button></a>	
+		</div>
+		<br>
+	</div>
+</div>
+<br>
--- searchProducts.php
+++ searchProducts.php
@@ -19,79 +19,53 @@ if (isset($_POST['searchItem'])){$_SESSION['searchItem'] = $_POST['searchItem'];
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</head>
-<body>
-	
-	<?php  include ("includes/nav.php"); ?>
-	
-	<div class="container"> 
+	<body>
+		<?php  include ("includes/nav.php"); ?>
 
-	<?php 
-	include ("includes/just-added.php"); 
-	if(isset($_GET['currentPage'])){$currentPage = $_GET['currentPage'];}
-	if(isset($_GET['category'])){
-		$rows = getCategoryItems($_GET['category'], (($currentPage-1)*5));
-		$noOfItems = getNoOfCategoryItems($_GET['category']);
-		unset($_SESSION['searchItem']);
-	} else {
-		$rows = getSearchItems($_SESSION['searchItem'], (($currentPage-1)*5));
-		$noOfItems = getNoOfSearchItems($_SESSION['searchItem']);
-	}
-	
-	$pages = ceil(($noOfItems)/5);  //rounds up
-	?>
-	  
-	<div class="col-md-9">
-		<div class="well">
-			<div class="row">
-				<div class="col-md-9">
-					<h4><?php if(isset($_GET['category'])){echo "'".$_GET['category'];} else {echo "'".$_SESSION['searchItem']; }echo "' - ".$noOfItems." Items";?></h4>
-				</div>		
-				<div class="col-md-3">
-					<h4><?php echo "Page ".$currentPage;?></h4>
-				</div>
-			</div>
-			<div class="row" align="center">
-				<h4><?php include ("includes/searchPagination.php");?></h4>
-			</div>
-		</div>
+		<div class="container"> 
 		
-	<?php		
-		foreach ($rows as $row) {
-		$product = new product($row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8]);
-	?>
-		
-		<div class="well">
-			<div class="row">
-				<div class="col-md-3">
-                <a href="viewProduct.php?<?php echo $product->getId(); ?>">
-					<img src="includes/<?php echo $product->getImg(); ?>" alt="Image of a product found from your search query" height="150" width="150">
-                    </a>
+			<?php 
+			include ("includes/just-added.php"); 
+			
+			if(isset($_GET['currentPage'])){$currentPage = $_GET['currentPage'];}
+			if(isset($_GET['category'])){
+				$rows = getCategoryItems($_GET['category'], (($currentPage-1)*5));
+				$noOfItems = getNoOfCategoryItems($_GET['category']);
+				unset($_SESSION['searchItem']);
+			} else {
+				$rows = getSearchItems($_SESSION['searchItem'], (($currentPage-1)*5));
+				$noOfItems = getNoOfSearchItems($_SESSION['searchItem']);
+			}
+			
+			$pages = ceil(($noOfItems)/5);  //rounds up
+			?>
+			  
+			<div class="col-md-9">
+				<div class="well">
+					<div class="row">
+						<div class="col-md-9">
+							<h4><?php if(isset($_GET['category'])){echo "'".$_GET['category'];} else {echo "'".$_SESSION['searchItem']; }echo "' - ".$noOfItems." Items";?></h4>
+						</div>		
+						<div class="col-md-3">
+							<h4><?php echo "Page ".$currentPage;?></h4>
+						</div>
+					</div>
+					<div class="row" align="center">
+						<h4><?php include ("includes/search-pagination.php");?></h4>
+					</div>
 				</div>
-				<div class="col-md-9">
-					<h5 class="pull-right"><?php if ($product->getPercentage() == 0){
-						echo "<strong> &pound;".$product->getPrice()."</strong>";
-					} else {
-						$salePriceTmp = number_format(($product->getPrice() * $product->getPercentage() / 100), 2, '.', '');
-						$salePrice =  number_format(($product->getPrice() - $salePriceTmp), 2, '.', '');
-						echo "<strong> Our Price: &pound;".$salePrice."</strong> RRP: <strike>&pound;".$product->getPrice() ."</strike><br>";
-					} ?> </h5> <!-- PLEASE IGNORE HTML ERRORS -->
-					<h4><a href="viewProduct.php?<?php echo $product->getId(); ?>"><?php echo $product->getName(); ?></a></h4>
-					<p> <?php echo $product->getDescription(); ?></p>
-					<a href="viewProduct.php?<?php echo $product->getId(); ?>"><button type="submit" name='itemId' value='<?php echo $product->getId(); ?>' class="btn btn-default right-margin"><i class="fa fa-eye"></i> <b> View </b> </button></a>	
-				</div>
-				<br>
+				
+			<?php		
+				foreach ($rows as $row) {
+					$product = new product($row[0], $row[1], $row[2], $row[3], $row[4], $row[5], $row[6], $row[7], $row[8]);
+					include ("includes/horizontal-item.php");
+
+				}
+				include ("includes/search-pagination.php");
+			?>
+			
+				
 			</div>
 		</div>
-	
-	<br>
-		
-	<?php
-		}
-		include ("includes/searchPagination.php");
-	?>
-	
-		
-	</div>
-    </div>
 	</body>
 </html>
