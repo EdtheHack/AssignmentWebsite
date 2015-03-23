739c8512e2d83af1add4146177d6db8675fc369b
jeremyclarke100 <jeremyclarke100@gmail.com>
2015-03-23 00:31:39 +0000

    Fixing slider

--- index.php
+++ index.php
@@ -40,26 +40,15 @@ include ("includes/nav.php");
 					<!-- Wrapper for slides -->
 					<div class="carousel-inner" role="listbox">
 						<div class="item active">
-							<img src="banner/banner1.png" alt="Image slider 1." height="300" width="735">
+							<img src="banner/banner1.png" alt="Image slider 1." style="width:735px;height:515px">
 						</div>
 						<div class="item">
-							<img src="banner/banner2.png" alt="Image slider 2." height="300" width="735">
+							<img src="banner/banner2.png" alt="Image slider 2." style="width:735px;height:515px">
 						</div>
                         <div class="item">
-							<img src="banner/banner4.png" alt="Image slider 3." height="300" width="735">
+							<img src="banner/banner4.png" alt="Image slider 3." style="width:735px;height:515px">
 						</div>
 					</div>
-					
-					<!-- Controls -->
-<!--					<a class="left carousel-control" href="#carousel-example-generic"
-						role="button" data-slide="prev"> <span
-						class="glyphicon glyphicon-chevron-left" aria-hidden="true"> </span>
-						<span class="sr-only">Previous</span>
-					</a> <a class="right carousel-control"
-						href="#carousel-example-generic" role="button" data-slide="next">
-						<span class="glyphicon glyphicon-chevron-right"
-						aria-hidden="true"></span> <span class="sr-only">Next</span>
-					</a> -->
 				</div>
 			</div>
 		</div>
