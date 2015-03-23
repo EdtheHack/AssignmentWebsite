e6e90a292ef3c699d33eb09a267111dc76b02041
Ed Hack <ed.hack@talktalk.net>
2015-03-23 02:28:23 +0000

    Changing footer to be bootstrap

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
-<div style="height:50px; background-color: #EEEEEE;">
+<footer class="footer">
 	<div class="container">
-		<p style="text-align: center">&copy;   i7212753 - i7219174 - i7261127 - i7259457</p>
+		<p class="text-muted" style="text-align: center">&copy;   i7212753 - i7219174 - i7261127 - i7259457</p>
 	</div>
-</div>
+</footer>
