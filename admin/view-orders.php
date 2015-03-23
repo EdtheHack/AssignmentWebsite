0e847c0f41bb2b79030c7cdc3de4b2a03cd4af73
Ashley Morris <ashleywm@outlook.com>
2015-03-23 01:30:48 +0000

    No echo

--- admin/view-orders.php
+++ admin/view-orders.php
@@ -104,7 +104,7 @@ include ("admin-nav.php");
 							<div class="modal-footer">
 								<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 								<form method="POST" action="view-orders.php">
-									<button type="button" name="delete" value="<?php $order[0]?>" class="btn btn-danger">Delete Order</button>
+									<button type="button" name="delete" value="<?php echo $order[0]?>" class="btn btn-danger">Delete Order</button>
 								</form>
 							</div>
 						</div>
